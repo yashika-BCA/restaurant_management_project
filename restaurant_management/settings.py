191e3583e60f2c746510b73b590c63a04799402e
yashika-BCA <yashikasorani06@gmail.com>
2025-08-12 16:47:57 +0530

    27038

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s^x_u!itekxd=@a3o3zv4d%hw$j6d#2v358wgltxl8rc(-^t&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
