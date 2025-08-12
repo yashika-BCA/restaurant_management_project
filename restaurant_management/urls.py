24c441912bd580defb32b685e9cf4df0e6108ee8
yashika-BCA <yashikasorani06@gmail.com>
2025-08-12 15:56:28 +0530

    27037

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
+    path('', include(home.urls)),
 ]
