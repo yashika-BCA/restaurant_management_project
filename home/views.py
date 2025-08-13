738f66cbf641d607a7cff96ae5fb9ae3c18cce43
yashika-BCA <yashikasorani06@gmail.com>
2025-08-13 21:31:25 +0530

    27040

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, home.html)
+    name = settings.RESTAURANT_NAME
+    return render(request, 'home.html' {'restaurant_namr': name})
 
 
 
