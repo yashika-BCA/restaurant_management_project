c8e9cc08bb1d375090376e54b55bdb20bec9c1bf
yashika-BCA <yashikasorani06@gmail.com>
2025-08-18 17:25:55 +0530

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+
+@api_view(['POST'])
+def staff_login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    if not email or not password:
+        return Response({'error': 'Email and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(request, username=email, password=password)
+
+    if user is not None:
+        return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 # Create your views here.
+
