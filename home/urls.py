434f446f9b3d73efc9421397671ebaf66f02f9f8
yashika-BCA <yashikasorani06@gmail.com>
2025-08-18 17:12:03 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from . import views
+from .views import (
+    create_restaurant,
+    list_restaurants,
+    get_restaurant,
+    update_restaurant,
+    delete_restaurant
+)
 
 urlpatterns = [
-    path('',views.homepage, name='homepage')
-    path('about/',views.about_us, name='about')
+    path('',views.homepage, name='homepage'),
+    path('about/',views.about_us, name='about'),
+    path('restaurants/', list_restaurants),
+    path('restaurants/create/', create_restaurant),
+    path('restaurants/<int:pk>/', get_restaurant),
+    path('restaurants/<int:pk>/update/', update_restaurant),
+    path('restaurants/<int:pk>/delete/', delete_restaurant),
+
 ]
